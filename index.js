199e258f3704d2258c57e63a4beed91ba76e72f8
BigLarder-com <90627865+BigLarder-com@users.noreply.github.com>
2023-02-02 19:10:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
-const { notFound, errorHandler } = require("./middlewares/errorHandler");
+const { notFound, errorHandler } = require("./middlewares/errorHandler")
 const app = express();
 const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 4000; // if port is not specified, we will use 4000
+const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
+const blogRouter = require("./routes/blogRoute");
+const categoryRouter = require("./routes/prodcategoryRoute");
+const blogcategoryRouter = require("./routes/blogCatRoute");
+const brandRouter = require("../routes/brandRoute");
+const couponRouter = require("../routes/couponRoute");
 const cookieParser = require("cookie-parser");
-
+const morgan = require("morgan");
 dbConnect();
-
-// use body parser To handle HTTP POST requests in Express.js
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-/* app.use("/", (req, res) => {
-  res.send("i am server.");
-}); */
-
+app.use(cookiePaser());
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
-
-// This middleware should be after all Routes
+app.use("/api/blog", blogRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/blogCategory", blogcategoryRouter);
+app.use("/api/brand", brandRouter);
+app.use("/api/coupon", couponRouter);
 app.use(notFound);
 app.use(errorHandler);
-
 app.listen(PORT, () => {
-  console.log("server is running on port : " + PORT);
+  console.log(`Server is running at PORT ${PORT}`);
 });
